3093c762d6c134a07477716e33ab23cff53241b4
Rafael Nascimento <158782579+RafaelGwent@users.noreply.github.com>
2024-02-03 20:09:10 -0300

    Update NotasDaProva.java

--- NotasDaProva.java
+++ NotasDaProva.java
@@ -1,22 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package notas.da.prova;
 
 import java.util.Scanner;
 
-/**
- *
- * @author rafae
- */
 public class NotasDaProva {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         Scanner scan = new Scanner(System.in);
         
         int x = scan.nextInt();
